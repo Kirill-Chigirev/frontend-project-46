88734aa02870cb0e326ce68a8dc13763bb839768
Kirill-Chigirev <chigirevk@bk.ru>
2022-12-17 14:31:46 +0700

    added index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('genDiff', () => {
+  const result = readFile('result.txt');
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
+});
