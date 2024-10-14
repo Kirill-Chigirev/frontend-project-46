14e0e53458e2adc090a3e4f1af13f973efeeb11f
Kirill Chigirev <chigirevk@bk.ru>
2024-10-14 19:37:58 +0700

    fixed linting errors

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -3,7 +3,9 @@ import parser from '../src/parser.js';
 let parsedFile;
 
 beforeAll(() => {
-  parsedFile = { host: 'hexlet.io', timeout: 50, proxy: '123.234.53.22', follow: false };
+  parsedFile = {
+    host: 'hexlet.io', timeout: 50, proxy: '123.234.53.22', follow: false,
+  };
 });
 
 test('parser', () => {
