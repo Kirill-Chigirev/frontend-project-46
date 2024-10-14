87bb6485df4b777b85b06245482527842319864d
Kirill Chigirev <chigirevk@bk.ru>
2024-10-14 19:31:01 +0700

    added fixtures & test for parser.js

---
+++ __tests__/parser.test.js
@@ -0,0 +1,12 @@
+import parser from '../src/parser.js';
+
+let parsedFile;
+
+beforeAll(() => {
+  parsedFile = { host: 'hexlet.io', timeout: 50, proxy: '123.234.53.22', follow: false };
+});
+
+test('parser', () => {
+  expect(parser('file1.json')).toEqual(parsedFile);
+  expect(parser('file1.yml')).toEqual(parsedFile);
+});
